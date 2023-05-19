80bd9052a4fe0159cb668958a0640d1947950b71
Fabio <fabio.tijerino23@gmail.com>
2023-05-19 02:09:56 -0600

    Principle Classes of the Proyect

---
+++ Models/Domain/AgeRating.cs
@@ -0,0 +1,19 @@
+﻿namespace ProyectoVideoteca.Models.Domain
+{
+    public class AgeRating
+    {
+        public int id { get; set; }
+        public int ageRating { get; set; }
+
+        public AgeRating()
+        {
+
+        }
+        public AgeRating(int id, int ageRating)
+        {
+            this.id = id;
+            this.ageRating = ageRating;
+
+        }
+    }
+}
---
+++ Models/Domain/Country.cs
@@ -0,0 +1,19 @@
+﻿namespace ProyectoVideoteca.Models.Domain
+{
+    public class Country
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Country()
+        {
+
+        }
+        public Country(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+
+        }
+    }
+}
