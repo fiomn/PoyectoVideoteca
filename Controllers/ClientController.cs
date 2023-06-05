12eedc6e0d7a39916f891df6b90e0d707b64c97d
fiomn <fio.mn1911@gmail.com>
2023-06-05 15:26:16 -0600

    Revert "forget password incompleto"
    This reverts commit 4eb472e33d6e054a432ff1453df8385c142a588d.

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -6,7 +6,6 @@ using Microsoft.Identity.Client;
 using ProyectoVideoteca.Data;
 using ProyectoVideoteca.Models;
 using ProyectoVideoteca.Models.DTO;
-using System.Net;
 using System.Net.Mail;
 
 namespace ProyectoVideoteca.Controllers
@@ -27,40 +26,16 @@ namespace ProyectoVideoteca.Controllers
             return View(user);
         }
 
-        //recovery password by email
-        public static void sendEmail(string email)
+        [HttpGet]
+        public ActionResult recoveryPassword()
         {
-            string emailSend = "fio.mn1911@gmail.com";
-            string pwdSend = "Navarro19!!!";
-
-            var fromAddress = new MailAddress(emailSend);
-            var toAddress = new MailAddress(email);
-            string subject = "Password recovery";
-            string body = "This is your new password for QStream";
-
-            var smtp = new SmtpClient
-            {
-                Host = "smt.gamil.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = true,
-                Credentials = new NetworkCredential(emailSend, pwdSend)
-            };
-
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = subject,
-                Body = body,
-            })
-            {
-                message.IsBodyHtml = false;
-                smtp.Send(message);
-            }
+            return View();
         }
 
-        public IActionResult recoveryPassword()
+        [HttpPost]
+        public ActionResult recoveryPassword(string email)
         {
+
             return View();
         }
     }
