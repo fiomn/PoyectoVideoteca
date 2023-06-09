458b93b5436176afac0d520be156e9bb46c1e7ff
fiomn <fio.mn1911@gmail.com>
2023-06-09 01:23:26 -0600

    cambios en admin y superAdmin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -11,6 +11,7 @@ using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using ProyectoVideoteca.Models.DTO;
 using ProyectoVideoteca.Repositories.Abstract;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProyectoVideoteca.Controllers
 {
@@ -18,11 +19,13 @@ namespace ProyectoVideoteca.Controllers
     public class AdminController : Controller
     {
         private TestUCRContext db = new TestUCRContext(); //database context
-
         private readonly IUserAuthenticationService _service; //database context authentication
-        public AdminController(IUserAuthenticationService service)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(IUserAuthenticationService service, UserManager<ApplicationUser> userManager)
         {
             this._service = service;
+            this._userManager = userManager;
         }
 
         public ActionResult AdminMain()
@@ -178,6 +181,116 @@ namespace ProyectoVideoteca.Controllers
 
             var stream = new MemoryStream(documentpdf); //save pdf in memory
             return File(stream, "application/pdf", "UsersList.pdf"); //name and type of pdf
+        }
+
+        public async Task<ActionResult> editProfile()
+        {
+            try
+            {
+                ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+                string username = user.UserName;
+                var userByName = new tb_USER();
+                var parameter = new SqlParameter("@username", username);
+
+                userByName = db.tb_USER.FromSqlRaw(@"exec getUserByName @username", new SqlParameter("@username", username)).AsEnumerable().FirstOrDefault();
+                ManagementUsers.users = userByName;
+                return View(userByName);
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        public ActionResult editUser(string userName)
+        {
+            var user = db.tb_USER.Find(userName);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> editUser(tb_USER user, RegistrationModel model)
+        {
+            try
+            {
+                var result = await _service.EditAsync(model); //save in context auth
+                db.tb_USER.Update(user); //save users in testUCR
+                db.SaveChanges();
+                return RedirectToAction(nameof(editProfile));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadProfileImage(IFormFile file)
+        {
+            // Obtén el usuario actual
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user != null)
+            {
+                // Verifica si se seleccionó un archivo
+                if (file != null && file.Length > 0)
+                {
+                    // Convierte el archivo en una cadena base64
+                    string profilePictureBase64;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(memoryStream);
+                        byte[] fileBytes = memoryStream.ToArray();
+                        profilePictureBase64 = Convert.ToBase64String(fileBytes);
+                    }
+
+                    // Actualiza la imagen de perfil en el modelo del usuario
+                    var userBD = db.tb_USER.FromSqlRaw(@"exec getUserByName @username", new SqlParameter("@username", user.UserName)).AsEnumerable().FirstOrDefault();
+                    userBD.IMG = profilePictureBase64;
+                    user.ProfilePicture = profilePictureBase64;
+
+                    // Guarda los cambios en la base de datos
+                    var result = await _userManager.UpdateAsync(user);
+                    db.tb_USER.Update(userBD);
+                    db.SaveChanges();
+                    if (!result.Succeeded)
+                    {
+                        // El cambio de correo electrónico no fue exitoso, agrega errores de validación.
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                        return View("ChangeProfilePicture");
+                    }
+                }
+            }
+            return RedirectToAction(nameof(editUser));
+        }
+
+        public async Task<IActionResult> ChangeProfilePicture()
+        {
+            // Obtener el usuario actualmente autenticado
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user != null)
+            {
+                string profilePictureBase64 = user.ProfilePicture;
+                // Verificar si la imagen de perfil existe
+                if (!string.IsNullOrEmpty(user.ProfilePicture))
+                {
+                    // Convertir la cadena base64 en una URL válida
+                    string imageFormat = "image/png"; // Cambia esto según el formato de imagen que estés utilizando
+                    string profilePictureUrl = $"data:{imageFormat};base64,{profilePictureBase64}";
+                    // Pasar la URL de la imagen de perfil a la vista
+                    ViewBag.ProfilePicture = profilePictureUrl;
+                }
+                else
+                {
+                    ViewBag.ProfilePicture = "https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png";
+                }
+            }
+
+            return View();
         }
     }
 }
--- Controllers/SuperAdminController.cs
+++ Controllers/SuperAdminController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Differencing;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using ProyectoVideoteca.Data;
 using ProyectoVideoteca.Models;
+using ProyectoVideoteca.Models.Domain;
 using ProyectoVideoteca.Models.DTO;
 using ProyectoVideoteca.Repositories.Abstract;
 using QuestPDF.Fluent;
@@ -16,9 +19,12 @@ namespace ProyectoVideoteca.Controllers
     {
         private TestUCRContext db = new TestUCRContext(); //database context
         private readonly IUserAuthenticationService _service; //database context authentication
-        public SuperAdminController(IUserAuthenticationService service)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SuperAdminController(IUserAuthenticationService service, UserManager<ApplicationUser> userManager)
         {
             this._service = service;
+            this._userManager = userManager;
         }
         public ActionResult SuperAdminMain()
         {
@@ -142,6 +148,116 @@ namespace ProyectoVideoteca.Controllers
 
             var stream = new MemoryStream(documentpdf); //save pdf in memory
             return File(stream, "application/pdf", "UsersList.pdf"); //name and type of pdf
+        }
+
+        public async Task<ActionResult> editProfile()
+        {
+            try
+            {
+                ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+                string username = user.UserName;
+                var userByName = new tb_USER();
+                var parameter = new SqlParameter("@username", username);
+
+                userByName = db.tb_USER.FromSqlRaw(@"exec getUserByName @username", new SqlParameter("@username", username)).AsEnumerable().FirstOrDefault();
+                ManagementUsers.users = userByName;
+                return View(userByName);
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        public ActionResult editUser(string userName)
+        {
+            var user = db.tb_USER.Find(userName);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> editUser(tb_USER user, RegistrationModel model)
+        {
+            try
+            {
+                var result = await _service.EditAsync(model); //save in context auth
+                db.tb_USER.Update(user); //save users in testUCR
+                db.SaveChanges();
+                return RedirectToAction(nameof(editProfile));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadProfileImage(IFormFile file)
+        {
+            // Obtén el usuario actual
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user != null)
+            {
+                // Verifica si se seleccionó un archivo
+                if (file != null && file.Length > 0)
+                {
+                    // Convierte el archivo en una cadena base64
+                    string profilePictureBase64;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(memoryStream);
+                        byte[] fileBytes = memoryStream.ToArray();
+                        profilePictureBase64 = Convert.ToBase64String(fileBytes);
+                    }
+
+                    // Actualiza la imagen de perfil en el modelo del usuario
+                    var userBD = db.tb_USER.FromSqlRaw(@"exec getUserByName @username", new SqlParameter("@username", user.UserName)).AsEnumerable().FirstOrDefault();
+                    userBD.IMG = profilePictureBase64;
+                    user.ProfilePicture = profilePictureBase64;
+
+                    // Guarda los cambios en la base de datos
+                    var result = await _userManager.UpdateAsync(user);
+                    db.tb_USER.Update(userBD);
+                    db.SaveChanges();
+                    if (!result.Succeeded)
+                    {
+                        // El cambio de correo electrónico no fue exitoso, agrega errores de validación.
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                        return View("ChangeProfilePicture");
+                    }
+                }
+            }
+            return RedirectToAction(nameof(editUser));
+        }
+
+        public async Task<IActionResult> ChangeProfilePicture()
+        {
+            // Obtener el usuario actualmente autenticado
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user != null)
+            {
+                string profilePictureBase64 = user.ProfilePicture;
+                // Verificar si la imagen de perfil existe
+                if (!string.IsNullOrEmpty(user.ProfilePicture))
+                {
+                    // Convertir la cadena base64 en una URL válida
+                    string imageFormat = "image/png"; // Cambia esto según el formato de imagen que estés utilizando
+                    string profilePictureUrl = $"data:{imageFormat};base64,{profilePictureBase64}";
+                    // Pasar la URL de la imagen de perfil a la vista
+                    ViewBag.ProfilePicture = profilePictureUrl;
+                }
+                else
+                {
+                    ViewBag.ProfilePicture = "https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png";
+                }
+            }
+
+            return View();
         }
     }
 }
