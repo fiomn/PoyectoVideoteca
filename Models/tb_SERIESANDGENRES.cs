4c41d80a3049151305b0954c9de8d1f26dd93b14
Fabio <fabio.tijerino23@gmail.com>
2023-06-10 01:19:43 -0600

    Carouseles de Series Añadidos Mejoras y Correciones carouseles en general

---
+++ Models/tb_SERIESANDGENRES.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoVideoteca.Models
+{
+    public class tb_SERIESANDGENRES
+    {
+
+        public List<tb_SERIE> series { get; set; }
+
+        public List<tb_GENRE> genres { get; set; }
+
+        public tb_SERIESANDGENRES(List<tb_SERIE> series, List<tb_GENRE> genres)
+        {
+
+            this.series = series;
+            this.genres = genres;
+
+        }
+    }
+}
