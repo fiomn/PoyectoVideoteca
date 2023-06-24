dda854564b1f75d0b8b2cf6d5aa009a328a01214
fiomn <fio.mn1911@gmail.com>
2023-06-23 19:32:22 -0600

    cambios en user

--- Models/tb_USER.cs
+++ Models/tb_USER.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoVideoteca.Models;
 
@@ -7,12 +8,16 @@ public partial class tb_USER
 {
     public string USERNAME { get; set; } = null!;
 
+    [Required]
+    [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#$^+=!*()@%&]).{8,}$", ErrorMessage = "Minimun lenght 8 and must contain 1 UpperCase, 1 LowerCase, 1 Special Character and 1 Digit")]
     public string PASSWORD { get; set; } = null!;
 
     public string NAME { get; set; } = null!;
 
     public string EMAIL { get; set; } = null!;
 
+    [Required]
+    [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#$^+=!*()@%&]).{8,}$", ErrorMessage = "Minimun lenght 8 and must contain 1 UpperCase, 1 LowerCase, 1 Special Character and 1 Digit")]
     public string? PASSWORD_CONFIRM { get; set; }
 
     public string ROLE { get; set; } = null!;
